5599c5c773c82cf3821222316d3897db93a31741
sudeep prajapati <sudeeppprajapati@gmail.com>
2025-08-12 19:12:52 +0530

    Wrap Providers component with SessionProvider and ImageKitProvider

--- app/components/Providers.tsx
+++ app/components/Providers.tsx
@@ -3,15 +3,23 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "sonner";
 import { ReactNode, useState } from "react";
+import { SessionProvider } from "next-auth/react";
+import { ImageKitProvider } from "@imagekit/next";
+
+const urlEndPoint = process.env.NEXT_PUBLIC_URL_ENDPOINT!;
 
 export default function Providers({ children }: { children: ReactNode }) {
     // Create QueryClient only once on the client
     const [queryClient] = useState(() => new QueryClient());
 
     return (
-        <QueryClientProvider client={queryClient}>
-            {children}
-            <Toaster richColors position="bottom-right" />
-        </QueryClientProvider>
+        <SessionProvider refetchInterval={5 * 60}>
+            <ImageKitProvider urlEndpoint={urlEndPoint}>
+                <QueryClientProvider client={queryClient}>
+                    {children}
+                    <Toaster richColors position="bottom-right" />
+                </QueryClientProvider>
+            </ImageKitProvider>
+        </SessionProvider>
     );
 }
